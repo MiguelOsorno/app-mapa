019600d0d7d2c854b4cb397f8e98d027207ad0c6
Miguel Osorno <miguel.osornolinares@gmail.com>
2020-07-20 19:57:56 -0500

    se agrego guardado de los marcadores en memoria

--- src/app/components/mapa/mapa.component.ts
+++ src/app/components/mapa/mapa.component.ts
@@ -13,13 +13,28 @@ export class MapaComponent implements OnInit {
   // lat = 51.678418;
   // lng = 7.809007;
 
+  indice = 0;
+
   marcadores: Marcador[] = [];
 
   arrayDeMarcadores: Mapboxgl.Marker[] = [];
 
   mapa: Mapboxgl.Map;
 
-  constructor() { }
+  constructor() {}
+
+
+   crearMarcadoresDeStorage(){
+    this.marcadores.forEach( (marcador) => {
+      this.crearMarcador(marcador.lng, marcador.lat);
+      this.establecerEventoEnMarcadores(this.indice);
+      this.indice++;
+    });
+
+    // this.establecerEventoEnMarcadores();
+   }
+
+
 
   ngOnInit() {
     (Mapboxgl as any ).accessToken = environment.maxboxKey;
@@ -35,12 +50,25 @@ export class MapaComponent implements OnInit {
 
       this.marcadores.push(nuevoMarcador);
 
+      this.guardarStorage();
+
       this.crearMarcador( nuevoMarcador.lng, nuevoMarcador.lat);
 
-      this.marcadores.forEach((marcador) => {
-        this.establecerEventoEnMarcadores();
-      });
+      this.establecerEventoEnMarcadores(this.indice);
+
+      this.indice++;
+
+      // this.establecerEventoBotonesMarcadores();
+
     });
+
+    if ( localStorage.getItem('marcadores') ){
+      this.marcadores = JSON.parse(localStorage.getItem('marcadores'));
+      if (this.marcadores.length > 0){
+        this.crearMarcadoresDeStorage();
+      }
+    }
+
   }
 
   crearMarcador(lng: number, lat: number){
@@ -64,16 +92,49 @@ export class MapaComponent implements OnInit {
   }
 
 
-  establecerEventoEnMarcadores(){
-    let contador = 0;
+  establecerEventoEnMarcadores(indice: number){
+
+    console.log('Se le agrego evento mouse enter al numero:', indice);
+    const marcador: Mapboxgl.Marker = this.arrayDeMarcadores[indice];
+    marcador.getElement().addEventListener('mouseenter', () => {
+      if (!marcador.getPopup().isOpen()){
+        marcador.togglePopup();
+      }
+    });
+
+    /*
     this.arrayDeMarcadores.forEach((marcador: Mapboxgl.Marker) => {
-      contador++;
       marcador.getElement().addEventListener('mouseenter', () => {
        if (!marcador.getPopup().isOpen()){
             marcador.togglePopup();
+            this.establecerEventoBotonesMarcadores();
        }
       });
-    });
+    });*/
+
+  }
+
+  /*establecerEventoBotonesMarcadores(){
+    let botonesBorrar = document.getElementsByClassName('btn-oranje');
+    console.log(botonesBorrar);
+    for (let contador = 0; contador < botonesBorrar.length; contador++){
+      if (botonesBorrar.item(contador)){
+        botonesBorrar.item(contador).addEventListener('click', () => this.borrar(contador));
+      }
+    }
+  }*/
+
+
+  guardarStorage(){
+    console.log('numero de elemento', this.marcadores.length);
+    localStorage.setItem('marcadores', JSON.stringify( this.marcadores ) );
+  }
+
+  borrar(i: number){
+   /* console.log('hola');
+    this.marcadores.splice(i, 1);
+    this.guardarStorage();
+    console.log(this.arrayDeMarcadores[i]);*/
   }
 
 }
